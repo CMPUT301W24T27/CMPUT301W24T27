184802dce1c00de163e0a175dd899a2c9697074e
natewu <36091727+natewu@users.noreply.github.com>
2024-02-01 17:46:57 -0700

    circle

---
+++ app/src/main/java/com/example/cmput301w24t27/Circle.java
@@ -0,0 +1,10 @@
+package com.example.cmput301w24t27;
+
+public class Circle extends Shape {
+    public int radius;
+
+    public Circle(int x, int y, int radius) {
+        super(x, y);
+        this.radius = radius;
+    }
+}
